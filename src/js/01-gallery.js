68ab11ba3fcaae47e19ed27769f03f0b5522295b
Silviu Barbu <s.barbu@goit.global>
2024-06-26 21:35:30 +0300

    Added simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listElement = document.querySelector('.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+  return element
+    .map(({ preview, original, description }) => {
+      return `<a class='gallery__link' href="${preview}">
+            <img class="gallery__image" src=${original} alt=${description}/>
+            </a>
+        `;
+    })
+    .join('');
+}
+
+listElement.insertAdjacentHTML('beforeend', photosMarkup);
+
+const galleryHandler = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galleryHandler.on('show.simplelightbox');
